6af11aee51a2f5cb07a7cbbd267d446002bbd67f
Joshua Starling <joshstarling97@gmail.com>
2019-10-04 22:19:46 -0400

    fixed bug, taken unneeded router

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,22 +4,20 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <Router>
-      <div>
-        <div className="App">
-          <h1>React Router Mini</h1>
-          <div>
-            <Link to="/">Home</Link>
-          </div>
-          <div>
-            <Link to="/about">About</Link>
-          </div>
-          <div>
-            <Link to="/contact">Contact</Link>
-          </div>
+    <div>
+      <div className="App">
+        <h1>React Router Mini</h1>
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <Link to="/about">About</Link>
+        </div>
+        <div>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
-    </Router>
+    </div>
   );
 };
 
